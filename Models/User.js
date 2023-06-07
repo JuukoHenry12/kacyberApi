73af7985fef25cf5b33478da09db480ff3b3c33a
unknown <MOICT & NG@DESKTOP-5RD7BU4.hub.local>
2023-06-07 11:43:49 +0300

    update

--- Models/User.js
+++ Models/User.js
@@ -1,22 +1,29 @@
 const mongoose = require('mongoose');
 const UserSchema= new mongoose.Schema({
-    name:{
+    firstname:{
         type:String,
         require:true
     },
+    surname:{
+        type:String,
+        require:true
+   },
+   phoneNumber:{
+         type:Number,
+         require:true
+    },
+
     email:{
         type:String,
         require:true
    },
-    phone:{
-         type:String,
-         require:true
+    selectedOption:{
+    type:String,
+    require:true
     },
-
     NinNumber:{
         type:String,
         require:true
-   },
-
+    },
 })
 module.exports = mongoose.model('User', UserSchema);
